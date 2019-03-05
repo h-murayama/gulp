9b04176cc8709abb70fd009e0fec743543c5cb3a
h-murayama <h-murayama@vectorinc.co.jp>
2019-03-05 21:11:09 +0900

    Initialize repository

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var scTop = 0;
+
+$(window).scroll(function(){
+  if($(this).scrollTop() > 100){
+    $('header').addClass('fixed');
+    if(!$('body').hasClass('mb')){
+      $('#go_top').addClass('fixed');
+    }
+  }else{
+    $('header').removeClass('fixed');
+    $('#go_top').removeClass('fixed');
+  }
+});
+
